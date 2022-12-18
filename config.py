690d3117867d2a8344f8c887e2aaaffdf8cc2f45
shanin-ivan <shanin-i2011@yandex.ru>
2022-12-18 18:00:50 +0300

    add status of order in orders page

--- config.py
+++ config.py
@@ -2,8 +2,9 @@ import os
 
 
 class Config:
+    # localhost 'postgresql://postgres:postgres@127.0.0.1:5432/flask-delivery'
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')\
-                              or 'postgresql://postgres:postgres@127.0.0.1:5432/flask-delivery'
+                              or 'postgresql://shanin-ivan:JE5VjFRNwy2i@ep-damp-hall-087772.eu-central-1.aws.neon.tech/neondb'
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'very-difficult-key'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # TRAP_HTTP_EXCEPTIONS = True
